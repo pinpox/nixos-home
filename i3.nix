7978a64f5972d950f235638bfb027a96b5418ae2
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-10-02 19:01:02 +0200

    add rofi config

--- i3.nix
+++ i3.nix
@@ -155,18 +155,60 @@ in
     };
 
     programs.rofi = {
+
+      # General
       enable = true;
-      # TODO
-      # colors =
-      # br
+      package = pkgs.rofi.override{ plugins = [ pkgs.rofi-pass ]; };
+      font = "${vars.font} 8";
+      cycle = true;
+      # extraConfig = "";
+
+      # Geometry
+      fullscreen = true;
+      borderWidth = 0;
+      lines = 3;
+      padding= 400;
+      rowHeight = 2;
+
+      colors = {
+        window = {
+          background = "${vars.colors.base00}";
+          # foreground = "${vars.colors.base00}";
+          separator= "${vars.colors.base0D}";
+          border= "${vars.colors.base00}";
+        rows = {
+          normal = {
+            background = "${vars.colors.base00}";
+            foreground = "${vars.colors.base05}";
+            backgroundAlt = "${vars.colors.base00}";
+            highlight = {
+              background = "${vars.colors.base02}";
+              foreground = "${vars.colors.base07}";
+            };
+          };
+          active = {
+            background = "${vars.colors.base00}";
+            foreground = "${vars.colors.base0D}";
+            backgroundAlt = "${vars.colors.base00}";
+            highlight = {
+              background = "${vars.colors.base00}";
+              foreground = "${vars.colors.base0D}";
+            };
+          };
+          urgent = {
+            background = "${vars.colors.base00}";
+            foreground = "${vars.colors.base08}";
+            backgroundAlt = "${vars.colors.base00}";
+            highlight = {
+              background = "${vars.colors.base00}";
+              foreground = "${vars.colors.base08}";
+            };
+          };
+        }[
+      };
     };
 
     # TODO ssh client config
-
-
-
-
-
 
     services.blueman-applet.enable = true;
 
