4917b360fc3b13b98a3043dd9312f6f0cb389158
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-10-02 12:50:34 +0200

    put xdg settings in own file

---
+++ xdg.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, lib,  ... }:
+{
+  xdg = {
+    enable = true;
+    configFile = {
+      nvim = {
+        source = ./dotfiles/vim;
+        target = "nvim";
+        recursive = true;
+          # onChange = "nvim -c +PlugInstall +qall --headless";
+        };
+
+        nvim_theme = {
+          target = "nvim/colors/ansible-theme.vim";
+          source  = ./dotfiles/vim-theme;
+        };
+
+        mpris  ={
+          executable = true;
+          text = ''
+            #!${pkgs.bash}/bin/bash
+
+            # pecifying the icon(s) in the script
+            # This allows us to change its appearance conditionally
+            icon=""
+
+            player_status=$(playerctl status 2> /dev/null)
+            if [[ $? -eq 0 ]]; then
+              metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+            fi
+
+            # Foreground color formatting tags are optional
+            if [[ $player_status = "Playing" ]]; then
+              echo "%{F#D08770}$icon $metadata"       # Orange when playing
+            elif [[ $player_status = "Paused" ]]; then
+              echo "%{F#65737E}$icon $metadata"       # Greyed out info when paused
+            else
+              echo "%{F#65737E}$icon"                 # Greyed out icon when stopped
+            fi
+          '';
+
+          target = "polybar/mpris.sh";
+        };
+      };
+    };
+  }
