02404c9d63a5880ff4429df8c3273f4fb8e59ce3
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-10-02 19:08:28 +0200

    fix rofi colors and font

--- i3.nix
+++ i3.nix
@@ -2,94 +2,94 @@
 let
   vars = import ./vars.nix;
 in
-{
+  {
 
 
 
-      xsession.windowManager.i3 = {
-        enable = true;
+    xsession.windowManager.i3 = {
+      enable = true;
 
-        package = pkgs.i3-gaps;
-        config = {
-          menu = "rofi";
-          startup = [
-            {
-              command = "picom";
-              always = false;
-              notification = false;
+      package = pkgs.i3-gaps;
+      config = {
+        menu = "rofi";
+        startup = [
+          {
+            command = "picom";
+            always = false;
+            notification = false;
             }
 
             {
-              command = "nitrogen --restore";
-              always = true;
-              notification = false;
+            command = "nitrogen --restore";
+            always = true;
+            notification = false;
             }
             {
-              command = "nm-applet";
-              always = true;
-              notification = false;
+            command = "nm-applet";
+            always = true;
+            notification = false;
             }
 
-          ];
-          colors =
+            ];
+            colors =
             {
+            background = "#${vars.colors.base00}";
+            focused = {
+            background = "#${vars.colors.base0D}";
+            border = "#${vars.colors.base0D}";
+            childBorder = "#${vars.colors.base00}";
+            indicator = "#${vars.colors.base0D}";
+            text = "#${vars.colors.base00}";
+            };
+
+            focusedInactive = {
+            background = "#${vars.colors.base03}";
+            border = "#${vars.colors.base03}";
+            childBorder = "#${vars.colors.base00}";
+            indicator = "#${vars.colors.base00}";
+            text = "#${vars.colors.base00}";
+            };
+
+            unfocused = {
               background = "#${vars.colors.base00}";
-              focused = {
-                background = "#${vars.colors.base0D}";
-                border = "#${vars.colors.base0D}";
-                childBorder = "#${vars.colors.base00}";
-                indicator = "#${vars.colors.base0D}";
-                text = "#${vars.colors.base00}";
-              };
-
-              focusedInactive = {
-                background = "#${vars.colors.base03}";
-                border = "#${vars.colors.base03}";
-                childBorder = "#${vars.colors.base00}";
-                indicator = "#${vars.colors.base00}";
-                text = "#${vars.colors.base00}";
-              };
-
-              unfocused = {
-                background = "#${vars.colors.base00}";
-                border = "#${vars.colors.base03}";
-                childBorder = "#${vars.colors.base00}";
-                indicator = "#${vars.colors.base00}";
-                text = "#${vars.colors.base0D}";
-              };
-
-              urgent = {
-                background = "#${vars.colors.base08}";
-                border = "#${vars.colors.base08}";
-                childBorder = "#${vars.colors.base00}";
-                indicator = "#${vars.colors.base08}";
-                text = "#${vars.colors.base00}";
-              };
+              border = "#${vars.colors.base03}";
+              childBorder = "#${vars.colors.base00}";
+              indicator = "#${vars.colors.base00}";
+              text = "#${vars.colors.base0D}";
             };
 
-            floating = {
-
-              border = 2;
+            urgent = {
+              background = "#${vars.colors.base08}";
+              border = "#${vars.colors.base08}";
+              childBorder = "#${vars.colors.base00}";
+              indicator = "#${vars.colors.base08}";
+              text = "#${vars.colors.base00}";
             };
+          };
 
-            focus = {
-              followMouse = true;
-              forceWrapping = true;
-            };
+          floating = {
 
-            fonts = ["Source Code Pro Semibold 12px"];
+            border = 2;
+          };
 
-            bars = [];
+          focus = {
+            followMouse = true;
+            forceWrapping = true;
+          };
 
-            gaps = {
-              bottom = 5;
-              horizontal = 5;
-              inner = 5;
-              left = 5;
-              outer = 5;
-              right = 5;
-              top = 5;
-              vertical = 5;
+          fonts = ["Source Code Pro Semibold 12px"];
+
+          bars = [];
+
+          gaps = {
+            bottom = 5;
+            horizontal = 5;
+            inner = 5;
+            left = 5;
+            outer = 5;
+            right = 5;
+            top = 5;
+            vertical = 5;
             # smartBorders = "no_gaps";
             # smartGaps = "on";
           };
@@ -158,8 +158,7 @@ in
 
       # General
       enable = true;
-      package = pkgs.rofi.override{ plugins = [ pkgs.rofi-pass ]; };
-      font = "${vars.font} 8";
+      font = "${vars.font} 12px";
       cycle = true;
       # extraConfig = "";
 
@@ -171,43 +170,45 @@ in
       rowHeight = 2;
 
       colors = {
+
         window = {
-          background = "${vars.colors.base00}";
-          # foreground = "${vars.colors.base00}";
-          separator= "${vars.colors.base0D}";
-          border= "${vars.colors.base00}";
+          background = "#${vars.colors.base00}";
+          # foreground = "#${vars.colors.base00}";
+          separator= "#${vars.colors.base0D}";
+          border= "#${vars.colors.base00}";
+        };
+
         rows = {
           normal = {
-            background = "${vars.colors.base00}";
-            foreground = "${vars.colors.base05}";
-            backgroundAlt = "${vars.colors.base00}";
+            background = "#${vars.colors.base00}";
+            foreground = "#${vars.colors.base05}";
+            backgroundAlt = "#${vars.colors.base00}";
             highlight = {
-              background = "${vars.colors.base02}";
-              foreground = "${vars.colors.base07}";
+              background = "#${vars.colors.base02}";
+              foreground = "#${vars.colors.base07}";
             };
           };
           active = {
-            background = "${vars.colors.base00}";
-            foreground = "${vars.colors.base0D}";
-            backgroundAlt = "${vars.colors.base00}";
+            background = "#${vars.colors.base00}";
+            foreground = "#${vars.colors.base0D}";
+            backgroundAlt = "#${vars.colors.base00}";
             highlight = {
-              background = "${vars.colors.base00}";
-              foreground = "${vars.colors.base0D}";
+              background = "#${vars.colors.base00}";
+              foreground = "#${vars.colors.base0D}";
             };
           };
           urgent = {
-            background = "${vars.colors.base00}";
-            foreground = "${vars.colors.base08}";
-            backgroundAlt = "${vars.colors.base00}";
+            background = "#${vars.colors.base00}";
+            foreground = "#${vars.colors.base08}";
+            backgroundAlt = "#${vars.colors.base00}";
             highlight = {
-              background = "${vars.colors.base00}";
-              foreground = "${vars.colors.base08}";
+              background = "#${vars.colors.base00}";
+              foreground = "#${vars.colors.base08}";
             };
           };
         };
       };
     };
-  };
 
     # TODO ssh client config
 
