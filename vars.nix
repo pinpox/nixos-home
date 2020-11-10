038b712eaa22ffcdbce742e18d24662d846486de
Pablo Ovelleiro Corral <mail@pablo.tools>
2020-11-10 16:02:59 +0100

    clean up vars

--- vars.nix
+++ vars.nix
@@ -3,23 +3,23 @@
 
     scheme = "generated";
     author = "Pablo";
-    base00 = "0b0b14"; # 0b0b14
-    # base01 = "1b1b24"; #1b1b24
-    base01 = "13131a";
-    base02 = "262631"; # 262631
-    base03 = "3a3a47"; # 3a3a47
-    base04 = "69697c"; # 69697c
-    base05 = "babac8"; # babac8
-    base06 = "cfcfdd"; # cfcfdd
-    base07 = "ffffff"; # ffffff
-    base08 = "ff4040"; # ff4040
-    base09 = "ff9326"; # ff9326
-    base0A = "ffcb65"; # ffcb65
-    base0B = "9ceb4f"; # 9ceb4f
-    base0C = "18ffe0"; # 18ffe0
-    base0D = "31baff"; # 31baff
-    base0E = "9d8cff"; # 9d8cff
-    base0F = "3f3866"; # 3f3866
+
+    base00 = "0b0b14"; #0b0b14 color0
+    base01 = "13131a"; #13131a color1
+    base02 = "262631"; #262631 color2
+    base03 = "3a3a47"; #3a3a47 color3
+    base04 = "69697c"; #69697c color4
+    base05 = "babac8"; #babac8 color5
+    base06 = "cfcfdd"; #cfcfdd color6
+    base07 = "ffffff"; #ffffff color7
+    base08 = "ff4040"; #ff4040 color8
+    base09 = "ff9326"; #ff9326 color9
+    base0A = "ffcb65"; #ffcb65 color10
+    base0B = "9ceb4f"; #9ceb4f color11
+    base0C = "18ffe0"; #18ffe0 color12
+    base0D = "31baff"; #31baff color13
+    base0E = "9d8cff"; #9d8cff color14
+    base0F = "3f3866"; #3f3866 color15
 
   };
 
