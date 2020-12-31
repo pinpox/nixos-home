7831ca13640712486cd9e8559c8d2e00015614f4
Pablo Ovelleiro Corral <github@pablo.tools>
2020-12-31 02:23:27 +0100

    use correct hash

--- awesome.nix
+++ awesome.nix
@@ -3,8 +3,16 @@ let
   vars = import ./vars.nix;
   awesome-config = pkgs.fetchgit {
     url = "https://github.com/pinpox/dotfiles-awesome.git";
-    sha256 = "122myzf3ymssaz5shn0bdk84k62wnb664nvrkpng5kd01ainyndb";
+    # sha256 = "122myzf3ymssaz5shn0bdk84k62wnb664nvrkpng5kd01ainyndb";
     fetchSubmodules = true;
+  # "rev": "08189291a1d3cc906e80a8e422966849f03133dc",
+  # "date": "2020-12-31T02:15:58+01:00",
+  # "path": "/nix/store/3478f513255863pa9vz6cdxginq8sfcr-dotfiles-awesome",
+  sha256  = "1pcw382snncksk2ll6vv89iw95j0cia8xpm36xz8cmmpw7ciz87w";
+  # "fetchSubmodules": false,
+  # "deepClone": false,
+  # "leaveDotGit": false
+}
   };
 
 in {
