d72227d008cc1a54ee05bba63a1b5c146e6a91f0
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-01 23:10:46 +0100

    update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,14 +2,19 @@
   description = "User configurations for pinpox";
 
   inputs.nur.url = "github:nix-community/NUR";
+  inputs.dotfiles-awesome.url = "github:pinpox/dotfiles-awesome";
 
-  outputs = { self, nixpkgs, nur }: {
+  outputs = { self, nixpkgs, dotfiles-awesome, nur }: {
 
     nixosModules = {
 
       # Desktop configuration, includes GUI
       desktop = {
-        imports = [ ./home.nix { nixpkgs.overlays = [ nur.overlay ]; } ];
+        imports = [
+          ./home.nix
+          dotfiles-awesome.dotfiles
+          { nixpkgs.overlays = [ nur.overlay ]; }
+        ];
       };
 
       # Serevr user configuration, only CLI
