b4fc83f9a91dad1e6476c34b68eb166b524a2b18
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-03 20:47:38 +0100

    use take out overpass

--- gtk.nix
+++ gtk.nix
@@ -53,7 +53,7 @@ in {
     (self: super: {
 
       nerdfonts =
-        super.nerdfonts.override { fonts = [ "SourceCodePro" "Overpass" ]; };
+        super.nerdfonts.override { fonts = [ "SourceCodePro" ]; };
 
       generated-gtk-theme = self.stdenv.mkDerivation rec {
         name = "generated-gtk-theme";
