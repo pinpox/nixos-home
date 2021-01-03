4b4b941423a87641089b13e5708b535d350c076d
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-03 13:42:28 +0100

    remove fontconfig

--- fonts.nix
+++ fonts.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }: {
 
-  fonts.fontconfig.enable = true;
+  # fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
       (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
