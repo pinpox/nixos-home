cd35793ac61ba71013c26d1457878d88f2619353
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-04 15:32:06 +0100

    try gtk to noto

--- gtk.nix
+++ gtk.nix
@@ -53,7 +53,7 @@ in {
     (self: super: {
 
       nerdfonts =
-        super.nerdfonts.override { fonts = [ "Overpass" ]; };
+        super.nerdfonts.override { fonts = [ "Noto" ]; };
 
       generated-gtk-theme = self.stdenv.mkDerivation rec {
         name = "generated-gtk-theme";
@@ -78,7 +78,7 @@ in {
     enable = true;
 
     font = {
-      name = "Overpass Nerd Font 9";
+      name = "Noto Sans Nerd Font 12";
       package = pkgs.nerdfonts;
     };
 
