cafff4beb2d3dace2c2e2d4f63f6309763a57642
Pablo Ovelleiro Corral <github@pablo.tools>
2021-01-06 17:39:29 +0100

    set bat theme

--- shell.nix
+++ shell.nix
@@ -182,11 +182,17 @@
   # Bat
   programs.bat = {
     enable = true;
-    config = {
-      # TODO look up opionts
-      theme = "TwoDark";
-    };
-    # themes = { TODO };
+    config = { theme = "base16"; };
+
+    # TODO generate a theme based on the colors in vars.nix
+    # themes = {
+    #   dracula = builtins.readFile (pkgs.fetchFromGitHub {
+    #     owner = "dracula";
+    #     repo = "sublime"; # Bat uses sublime syntax for its themes
+    #     rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+    #     sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+    #   } + "/Dracula.tmTheme");
+    # }
   };
 
 }
