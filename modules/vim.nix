112981f820eb402591b17a7b56db046024719241
Pablo Ovelleiro Corral <github@pablo.tools>
2021-02-05 10:36:58 +0100

    disable treesitter

--- modules/vim.nix
+++ modules/vim.nix
@@ -2,13 +2,13 @@
 let vars = import ./vars.nix;
 in {
 
-  home.file = {
-    "${config.xdg.configHome}/nvim/parser/c.so".source =
-      "${pkgs.tree-sitter.builtGrammars.c}/parser";
+  # home.file = {
+  #   "${config.xdg.configHome}/nvim/parser/c.so".source =
+  #     "${pkgs.tree-sitter.builtGrammars.c}/parser";
 
-    "${config.xdg.configHome}/nvim/parser/go.so".source =
-      "${pkgs.tree-sitter.builtGrammars.go}/parser";
-  };
+  #   "${config.xdg.configHome}/nvim/parser/go.so".source =
+  #     "${pkgs.tree-sitter.builtGrammars.go}/parser";
+  # };
 
   programs.neovim = {
     enable = true;
