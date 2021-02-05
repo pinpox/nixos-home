0b8b8b496dc75d602abd573310039cb39b383260
Pablo Ovelleiro Corral <github@pablo.tools>
2021-02-05 08:58:22 +0100

    use pure from prezto

--- modules/shell.nix
+++ modules/shell.nix
@@ -103,6 +103,14 @@ in {
         "syntax-highlighting"
         "history-substring-search"
       ];
+
+      prompt = {
+        theme = "pure";
+        # showReturnVal = true;
+      };
+
+      terminal.autoTitle = =true;
+
     };
 
     plugins = [
@@ -138,13 +146,6 @@ in {
         src = builtins.fetchGit {
           url = "https://github.com/zsh-users/zsh-syntax-highlighting/";
           rev = "932e29a0c75411cb618f02995b66c0a4a25699bc";
-        };
-      }
-      {
-        name = "pure";
-        src = builtins.fetchGit {
-          url = "https://github.com/sindresorhus/pure";
-          rev = "8ec575c886c8bb33a87f80b9710ee5e379a0b589";
         };
       }
     ];
