ef2f6b060992357c723a2f4000fb19e95714e157
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-26 01:28:07 +0100

    remove deprecated chromium opts

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -1,19 +1,9 @@
 { config, pkgs, lib, ... }: {
   programs.chromium = {
     enable = true;
-    extraOpts = {
-      # "BrowserSignin" = 0;
-      # "SyncDisabled" = true;
-      "PasswordManagerEnabled" = false;
-      "SpellcheckEnabled" = true;
-      "SpellcheckLanguage" = [ "de" "en-US" ];
-    };
-
     extensions = [
-
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # Ublock Origin
-
     ];
   };
 }
