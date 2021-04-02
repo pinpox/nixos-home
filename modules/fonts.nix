bedfdc7003a900f907e697ac4b2585b79701838e
Pablo Ovelleiro Corral <github@pablo.tools>
2021-04-02 18:54:12 +0200

    Simplify fonts config

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,12 +1,3 @@
 { config, pkgs, lib, ... }: {
-
   fonts.fontconfig.enable = true;
-
-  home.packages = with pkgs; [
-    corefonts
-    (nerdfonts.override { fonts = [ "SourceCodePro" ]; })
-    noto-fonts-emoji
-    source-sans-pro
-    source-serif-pro
-  ];
 }
